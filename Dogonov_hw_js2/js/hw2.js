f103d4369d9c6d07a92dc5bf4787bddf4a8e8a8b
Dogonov <dogonov1996@gmail.com>
2022-06-20 22:07:07 +0300

    1

--- Dogonov_hw_js2/js/hw2.js
+++ Dogonov_hw_js2/js/hw2.js
@@ -1,14 +1,7 @@
-// 2. Построить прямоугольный треугольник состоящий из символа "*".
-// Запросить у пользователя число, к примеру - 4
-
-// *
-// **
-// ***
-// ****
 var num = +prompt("Введите число строк");
 var star = "*";
-var resalt = '';
+var result = '';
 for (var i = 1; i <= num; i++) {
-   var resalt = resalt + star;
-    console.log(resalt);
+   result = result + star;
+    console.log(result);
 }
